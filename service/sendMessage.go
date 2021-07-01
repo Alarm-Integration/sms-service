338efeabf0d55adb56b0db9f33da358c4acae5a0
gm2202988 <kmk@gabia.com>
2021-07-01 11:29:41 +0900

    test

--- service/sendMessage.go
+++ service/sendMessage.go
@@ -11,29 +11,32 @@ import (
 // 메세지 발송
 func SendGroupMessage(createGroupParams map[string]string, sendMessageDataList []model.SendMessageDto) error {
 	client := createClient()
-	client.Messages.Config = map[string]string{
-		"APIKey":    viper.GetString("sms.APIKey"),
-		"APISecret": viper.GetString("sms.APISecret"),
-		"Protocol":  viper.GetString("sms.Protocol"),
-		"Domain":    viper.GetString("sms.Domain"),
-		"Prefix":    "",
-	}
+	// client.Messages.Config = map[string]string{
+	// 	"APIKey":    viper.GetString("sms.APIKey"),
+	// 	"APISecret": viper.GetString("sms.APISecret"),
+	// 	"Protocol":  viper.GetString("sms.Protocol"),
+	// 	"Domain":    viper.GetString("sms.Domain"),
+	// 	"Prefix":    "",
+	// }
 
 	// 1. 메세지 발송을 위한 그룹 생성
 	groupId, createGroupErr := createGroup(createGroupParams)
 	if createGroupErr != nil {
+		fmt.Println("create group error")
 		return createGroupErr
 	}
 
 	// 2. 그룹에 메세지 데이터 저장
 	addMessageErr := addGroupMessage(groupId, sendMessageDataList)
 	if addMessageErr != nil {
+		fmt.Println("add message error")
 		return addMessageErr
 	}
 
 	// 3. 완성된 그룹의 메세지 발송
 	_, sendMessageErr := client.Messages.SendGroup(groupId)
 	if sendMessageErr != nil {
+		fmt.Println("send message error")
 		return sendMessageErr
 	}
 
@@ -57,13 +60,13 @@ func createClient() *coolsms.Client {
 // 메세지 발송을 위한 그룹 생성
 func createGroup(params map[string]string) (string, error) {
 	client := createClient()
-	client.Messages.Config = map[string]string{
-		"APIKey":    viper.GetString("sms.APIKey"),
-		"APISecret": viper.GetString("sms.APISecret"),
-		"Protocol":  viper.GetString("sms.Protocol"),
-		"Domain":    viper.GetString("sms.Domain"),
-		"Prefix":    "",
-	}
+	// client.Messages.Config = map[string]string{
+	// 	"APIKey":    viper.GetString("sms.APIKey"),
+	// 	"APISecret": viper.GetString("sms.APISecret"),
+	// 	"Protocol":  viper.GetString("sms.Protocol"),
+	// 	"Domain":    viper.GetString("sms.Domain"),
+	// 	"Prefix":    "",
+	// }
 	createdGroup, err := client.Messages.CreateGroup(params)
 	if err != nil {
 		return "", err
@@ -76,13 +79,13 @@ func createGroup(params map[string]string) (string, error) {
 // 생성된 그룹에 메세지 데이터 저장
 func addGroupMessage(groupId string, sendMessageDataList []model.SendMessageDto) error {
 	client := createClient()
-	client.Messages.Config = map[string]string{
-		"APIKey":    viper.GetString("sms.APIKey"),
-		"APISecret": viper.GetString("sms.APISecret"),
-		"Protocol":  viper.GetString("sms.Protocol"),
-		"Domain":    viper.GetString("sms.Domain"),
-		"Prefix":    "",
-	}
+	// client.Messages.Config = map[string]string{
+	// 	"APIKey":    viper.GetString("sms.APIKey"),
+	// 	"APISecret": viper.GetString("sms.APISecret"),
+	// 	"Protocol":  viper.GetString("sms.Protocol"),
+	// 	"Domain":    viper.GetString("sms.Domain"),
+	// 	"Prefix":    "",
+	// }
 	params := make(map[string]interface{})
 	params["messages"] = sendMessageDataList
 
