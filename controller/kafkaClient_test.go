266f717b1a51eee8d2c950ec51b4b458acc87a14
gm2202988 <kmk@gabia.com>
2021-07-08 10:22:30 +0900

    test: kafka client create error test - 배포 테스트를 위한 test 제거

--- controller/kafkaClient_test.go
+++ controller/kafkaClient_test.go
@@ -1,101 +1,66 @@
 package controller
 
 import (
-	"errors"
-	"fmt"
-	"testing"
-
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/h2non/gock.v1"
+	"testing"
 )
 
-func Test_test(t *testing.T) {
-	// Mock the expected outgoing request for new config
-	defer gock.Off()
-	gock.New("http://config-server:8888").
-		Get("/sms-service/test/master").
-		Reply(200).
-		BodyString(`{
-			"name":"sms-service",
-			"profiles":["test"],
-			"label":null,
-			"version":null,
-			"propertySources":[{
-				"name":"file:/config-repo/sms-service.yml",
-				"source":{
-					"server_port":6767,
-					"server_name":"SMS-SERVICE RELOADED"
-				}
-			}]
-		}`)
+func Test_Create_Consumer(t *testing.T) {
 
-	Convey("Given a refresh event received", t, func() {
-		// body := `{
-		// 	"type":"RefreshRemoteApplicationEvent",
-		// 	"timestamp":1494514362123,
-		// 	"originService":"config-server:docker:8888",
-		// 	"destinationService":"sms-service:**",
-		// 	"id":"53e61c71-cbae-4b6d-84bb-d0dcc0aeb4dc"
-		// }`
-		Convey("When handled", func() {
-			// handleRefreshEvent([]byte(body), SERVICE_NAME)
+	Convey("Given empty kafka configuration", t, func() {
+		config := &kafka.ConfigMap{}
 
-			Convey("Then Viper should have been re-populated with values from Source", func() {
-				// So(viper.GetString("server_port"), ShouldEqual, "6767")
-				// So(viper.GetString("server_name"), ShouldEqual, "SMS-SERVICE RELOADED")
+		Convey("When trying to connect with kafka server", func() {
+			consumer, err := createConsumer(config)
+
+			Convey("Then connection would be failed", func() {
+				assert.Nil(t, consumer)
+				assert.IsType(t, &kafka.Consumer{}, consumer)
+				assert.EqualError(t, err, createClientErr.Error())
 			})
 		})
 	})
 }
 
-func Test_Kafka_Connection_Fail(t *testing.T) {
+//func Test_Subscribe_Topics(t *testing.T) {
+//
+//	Convey("Given no groupId to kafka server", t, func() {
+//		consumer, _ := kafka.NewConsumer(&kafka.ConfigMap{
+//			"bootstrap.servers": "http://kafka-server:9092/",
+//			"auto.offset.reset": "earliest",
+//		})
+//		topics := []string{"test01", "test02"}
+//
+//		Convey("When trying to subscribing topics", func() {
+//			err := subscribeTopics(consumer, topics)
+//
+//			Convey("Then subscribing topics would be failed", func() {
+//				assert.EqualError(t, err, subscribeTopicErr.Error())
+//			})
+//		})
+//	})
+//}
 
-	// given
-	kafkaServer := "1234.1234.1234"
-	groupId := "sms-service"
-	topics := []string{"sms"}
-	expectedError := fmt.Sprintf("%s:9092/bootstrap: Failed to resolve '%s:9092':", kafkaServer, kafkaServer)
-
-	// when
-	err := ConnectKafkaConsumer(kafkaServer, groupId, topics, true)
-
-	// then
-	assert.NotNil(t, err)
-	assert.Contains(t, err.Error(), expectedError)
-
-}
-
-func Test_Kafka_Consumer_Create_Fail(t *testing.T) {
-
-	// given
-	kafkaServer := "139.150.75.240"
-	var groupId string
-	topics := []string{"sms"}
-	expectedErrorString := "[Kafka] consumer create failed"
-
-	// when
-	err := ConnectKafkaConsumer(kafkaServer, groupId, topics, true)
-
-	// then
-	assert.NotNil(t, err)
-	assert.EqualError(t, errors.New(expectedErrorString), expectedErrorString)
-
-}
-
-func Test_Kafka_Consumer_Subscribe_Fail(t *testing.T) {
-
-	// given
-	kafkaServer := "139.150.75.240"
-	groupId := ""
-	topics := []string{"sms"}
-	expectedErrorString := "[Kafka] consumer topic subscribe failed"
-
-	// when
-	err := ConnectKafkaConsumer(kafkaServer, groupId, topics, true)
-
-	// then
-	assert.NotNil(t, err)
-	assert.EqualError(t, errors.New(expectedErrorString), expectedErrorString)
-
-}
+//func Test_Kafka_Consumer_Subscribe_Faisl(t *testing.T) {
+//
+//	Convey("Given a groupId with empty string to kafka server", t, func() {
+//		kafkaServer := "139.150.75.240"
+//		//groupId := ""
+//		topics := []string{"sms"}
+//
+//		Convey("When trying to connect with kafka server", func() {
+//			err := ConnectKafkaConsumer(&kafka.ConfigMap{
+//				"bootstrap.servers": kafkaServer,
+//				//"group.id":          groupId,
+//				"auto.offset.reset": "earliest",
+//			}, topics, true)
+//
+//			Convey("Then subscribing topic message would be failed", func() {
+//				assert.NotNil(t, err)
+//				assert.EqualError(t, err, "[Kafka] consumer topic subscribe failed")
+//			})
+//		})
+//	})
+//}
