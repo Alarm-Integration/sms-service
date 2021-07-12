81d1b9f7ee7fbe40caa1582020e2ffc50a7c0f60
gm2202988 <kmk@gabia.com>
2021-07-12 11:29:02 +0900

    refactor: goconvey 적용

--- controller/eurekaClient_test.go
+++ controller/eurekaClient_test.go
@@ -1,39 +1,41 @@
 package controller
 
 import (
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func Test_Eureka_Registration_Success(t *testing.T) {
 
-	// given
-	defaultzone := "http://139.150.75.239:8761/eureka/"
-	app := "sms-service"
-	port := 30020
+	Convey("Given", t, func() {
+		defaultzone := "http://139.150.75.239:8761/eureka/"
+		app := "sms-service"
+		port := 30020
 
-	// when
-	err := ReigsterEurekaClient(defaultzone, app, port)
+		Convey("When", func() {
+			err := ReigsterEurekaClient(defaultzone, app, port)
 
-	// then
-	assert.Nil(t, err)
-
+			Convey("Then", func() {
+				So(err, ShouldBeNil)
+			})
+		})
+	})
 }
 
 func Test_Eureka_Registration_Fail(t *testing.T) {
 
-	// given
-	defaultzone := "http://139.150.75.2391234:8761/eureka/"
-	app := "sms-service"
-	port := 30020
-	expectedErrorString := "[Eureka] client registration failed"
+	Convey("Given", t, func() {
+		defaultzone := "http://139.150.75.2391234:8761/eureka/"
+		app := "sms-service"
+		port := 30020
+		expectedErr := "[Eureka] client registration failed"
 
-	// when
-	err := ReigsterEurekaClient(defaultzone, app, port)
+		Convey("When", func() {
+			err := ReigsterEurekaClient(defaultzone, app, port)
 
-	// then
-	assert.NotNil(t, err)
-	assert.EqualError(t, err, expectedErrorString)
-
+			Convey("Then", func() {
+				So(err, ShouldBeError, expectedErr)
+			})
+		})
+	})
 }
